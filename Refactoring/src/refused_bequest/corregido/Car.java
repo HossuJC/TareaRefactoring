bdef77eab457f4e896393891b500a42f456d3109
Josue Cabezas <josalcab@espol.edu.ec>
2020-01-11 02:13:46 -0500

    Ejemplo de Refused Bequest agregado

---
+++ Refactoring/src/refused_bequest/corregido/Car.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refused_bequest.corregido;
+
+/**
+ *
+ * @author Josue Cabezas
+ */
+public class Car extends Vehicle {
+
+    void Drive() {
+        
+    }
+
+}
